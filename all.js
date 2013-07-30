48d242aebbec3b3e50a7fec5d92a8198836585b2
Joshua T Kalis <kalisjoshua@gmail.com>
2013-07-30 17:36:02 -0400

    add location information

--- all.js
+++ all.js
@@ -4,33 +4,48 @@ var APICraft = (function APICraft_API_closure (baseURL) {
     return baseURL + endpoint;
   };
 }('http://api.api-craft.org/conferences/detroit2013/'));
- 
+
 $.fn.ready(function ($) {
   var DAYNAMES
     , DAYNAMESLONG
     , TEMPLATE
     , TODAYSDAYNAME
-    , TODAYSDAYNAMELONG;
- 
+    , TODAYSDAYNAMELONG
+    , YEAR;
+
   DAYNAMES = 'Sun Mon Tue Wed Thu Fri Sat'.split(' ');
   DAYNAMESLONG = 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split(' ');
 
   TEMPLATE = Handlebars
-    .compile('{{#each events}}<div><h2>{{title}}</h2><p>{{start}} - {{end}}</p></div>{{/each}}');
+    .compile('{{#each events}}<div><h3>{{title}} <small>@{{location}}</small></h3><p>{{{detail}}}</p></div>{{/each}}');
 
   TODAYSDAYNAME = (/\w+/)
     .exec(new Date())[0];
- 
+
   TODAYSDAYNAMELONG = DAYNAMESLONG[DAYNAMES.indexOf(TODAYSDAYNAME)];
- 
+
+  YEAR = (new Date()).getFullYear();
+
   $.get(APICraft('agenda'), function (res) {
     var todays_events;
- 
+
     todays_events = res.agenda
-      .filter(function (item) {
-        return ~item.end.indexOf(TODAYSDAYNAMELONG);
-      });
- 
+      .reduce(function (acc, item) {
+        if (~item.end.indexOf(TODAYSDAYNAMELONG)) {
+          item.detail = item.start
+            .replace(YEAR, YEAR + '<br />');
+
+          item.detail += ' - ' + item.end
+            .split(' ')
+            .slice(-2)
+            .join(' ');
+
+          acc.push(item);
+        }
+
+        return acc;
+      }, []);
+
     $('article')
       .html(TEMPLATE({'events': todays_events}));
   });
